bf09ff445ccc83e7ef8c4ea0a52c7955f3420713
nikop111997-blip <nikop111997@gmail.com>
2026-06-01 18:00:52 +0530

    first commitss

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    // Tells Next.js not to bundle these specifically
+    serverComponentsExternalPackages: [
+      'playwright-core', 
+      '@sparticuz/chromium-min'
+    ],
+    // Explicitly include the playwright-core files in the serverless function output
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./node_modules/playwright-core/**/*'],
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
